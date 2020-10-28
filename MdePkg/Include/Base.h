12a1b91ff8c48dddfeae16231566ecaf6166fdd2
pcfist <pcfist@gmail.com>
2020-10-28 23:22:23 +0300

    MdePkg: fix `STATIC_ASSERT` definition for C++ code.

--- MdePkg/Include/Base.h
+++ MdePkg/Include/Base.h
@@ -799,7 +799,7 @@ typedef UINTN  *BASE_LIST;
 **/
 #ifdef MDE_CPU_EBC
   #define STATIC_ASSERT(Expression, Message)
-#elif defined(_MSC_EXTENSIONS)
+#elif defined(_MSC_EXTENSIONS) || defined(__cplusplus)
   #define STATIC_ASSERT static_assert
 #else
   #define STATIC_ASSERT _Static_assert
